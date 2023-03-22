931a88c62ed4ee3585d0a9ded40d84d39c3460d4
ashori2002 <mohammadashori2002@gmail.com>
2023-03-23 02:27:40 +0430

    Variance

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 myList = [10,12,13,15,18,20,25,26,32]
 
 average = (sum(myList)/len(myList))
+print(f"Avrage : {average}")
+newList = []
+for i in myList:
+    if(i>average):
+        newList.append((i - average)**2)
+    elif(i<average):
+        newList.append((average - i)**2)
+    elif(i==average):
+        newList.append(0)
+    else:
+        print("ERROR")
+
+variance = sum(newList)/5
+print(f"Variance : {variance}")
+
+
